0140e649a5671ecb9500d106da2b2714cf9e5e84
Cameron Sanborn <cam02299@tjx.com>
2021-03-17 15:48:33 -0400

    added dependency initialization for passport.js, bodyParser, and code to sync database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const app = express();
-const http = require('http');
-const express = require('express');
+const app       = express();
+const http      = require('http');
+const express   = require('express');
+var models      = require('./app/models');
+var passport    = require('passport');
+var session     = require('express-session');
+var bodyParser  = require('body-parser');
 
 app.use(express.json());
 
@@ -15,6 +19,25 @@ app.use((req, res, next) => {
 
 require("./app/routers/customers.route")(app);
 require("./app/routers/orders.route")(app);
+
+//Initialize BodyParser (to be used with password encryption)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//For Passport Initialization
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true})); //session secret
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+
+// Load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+
+//Sync Database
+models.sequelize.sync().then(function() {
+  console.log('Nice! Database looks fine')
+}).catch(function(err) {
+  console.log(err, "Something went wrong with the Database Update!")
+});
 
 // default URL to API
 app.use('/', function(req, res) {
