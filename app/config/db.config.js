cab00d6583fb6bc5f09652b97766fed8a5b17a26
Cameron Sanborn <cam02299@tjx.com>
2021-03-17 15:12:57 -0400

    testing git ignore

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "RooT0000",
-  DB: "ordersdb",
+  PASSWORD: "[password]",
+  DB: "tjxcapstone",
   dialect: "mysql",
   pool: {
     max: 5,
