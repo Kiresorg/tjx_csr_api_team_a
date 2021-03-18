0334314fedd281c03f2de85eabf81d21796b7810
Corrin Courville <cor02235@tjx.com>
2021-03-18 10:10:41 -0400

    Implement CRU

--- app/controllers/customers.controller.js
+++ app/controllers/customers.controller.js
@@ -1,10 +1,11 @@
 const dB = require("../models/index");
 const Customer = dB.customer;
 const db = require("mysql2");
+const { USER } = require("../config/db.config");
+const { sequelize } = require("../models/index");
 
 // create and save a Customer
 exports.create = (req, res) => {
-    //res.send("reached create new customer")
     // create customer
     const customer = new Customer({
         first_name: req.body.first_name,
@@ -29,7 +30,7 @@ exports.create = (req, res) => {
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Error while creating Customer"
+                    err.message + req.body.first_name || "Error while creating Customer"
             });
         })
         
@@ -54,71 +55,86 @@ exports.findAll = (req, res) => {
 
 // get Claim by id
 exports.findOne = (req, res) => {
-    const id = req.params.id;
-    res.send("reached get customer by id");
-    /**
-    Customer.findById(id)
+    Customer.findOne({ where: { id: req.params.id } })
         .then(data => {
             if(!data)
-                res.status(404).send({ message: "Not found: Claim with id of " + id });
+                res.status(404).send({ message: "Not found: Customer with id of " + id });
             else res.send(data);
         })
         .catch(err => {
             res
                 .status(500)
-                .send({ message: "Error retreiving Claim with id of " + id });
+                .send({ message: "Error retreiving Customer with id of " + id });
         });
-         */
 };
 
 // update Customer by id
 exports.update = (req, res) => {
-    res.send('Reached update customer by id');
-    /*if(!req.body) {
+    if(!req.body) {
         return res.status(400).send({
             message: "Empty data for update"
         });
     }
-
-    const id = req.params.id;
-
-    Claim.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    const id=req.params.id ;
+    Customer.update(
+        {
+        id: req.body.id,
+        first_name: req.body.first_name,
+        middle_name: req.body.middle_name ? req.body.middle_name : null,
+        last_name: req.body.last_name,
+        phone: req.body.phone,
+        email: req.body.email, 
+        notes: req.body.notes ? req.body.notes : null,
+        address_line1: req.body.address_line1 ? req.body.address_line1 : '',
+        address_line2: req.body.address_line2 ? req.body.address_line2 : '',
+        city: req.body.city ? req.body.city : '',
+        state: req.body.state ? req.body.state : '',
+        zip: req.body.zip ? req.body.zip : ''
+        },
+        {
+            where: { id :id }
+        })
         .then(data => {
             if (!data) {
                 res.status(404).send({
-                    message: `Unable to update Claim with id of ${id}.`
+                    message: `Unable to update Customer with id of ${id}.`
                 });
-            } else res.send({ message: "Claim updated successfully." });
+            } else {
+                res.send({ message: "Customer updated successfully." });
+            }
         })
         .catch(err => {
             res.status(500).send({
                 message: "Error while trying to update Claim with id of " + id
             });
         });
-        */
 };
 
 // delete Claim by id
 exports.delete = (req, res) => {
-    const id = req.params.id;
-    res.send("reached delete customer by id");
-    /*
-    Customer.findByIdAndRemove(id)
+    const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) 
+    {
+        res.status(404).send({
+            message: "Invalid ID parameter."
+        });
+    }
+    ;
+    return sequelize.query(`DELETE FROM customers WHERE id=${id}`)
         .then(data => {
-            if(!data) {
+            if (!data) {
                 res.status(404).send({
-                    message: `Unable to delete Claim with id of ${id}.`
+                    message: `Unable to delete Customer with id of ${id}.`
                 });
             } else {
                 res.send({
-                    message: "Claim deleted successfully."
-                })
+                    message: "Customer deleted successfully."
+                });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Unable to delete Claim with id of " + id
+                message: "Error with deleting Customer: " + err
             });
         });
-        */
 };
